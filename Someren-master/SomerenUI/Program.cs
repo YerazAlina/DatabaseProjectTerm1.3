e9527545154ee9b86018cd5920db8d3aed84a929
Yeraz Ayana <yerazalina@users.noreply.github.com>
2021-03-15 09:27:57 +0100

    firstVersion

--- Someren-master/SomerenUI/Program.cs
+++ Someren-master/SomerenUI/Program.cs
@@ -11,6 +11,7 @@ namespace SomerenUI
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
         [STAThread]
         static void Main()
         {
